80417bc0f8cd90a4df86c80f18653c9b83d09f58
Mauro Castrogiovanni <68161442+castrogiovanni83@users.noreply.github.com>
2023-04-02 18:28:13 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <conio.h>
+#include <time.h>
+#include "funciones.h"
+
+using namespace std;
+
+int main(){
+    system ("color E2");
+    int opcion,t=10,t2=11,t3=5,opc;
+    int punto[t2]={},dados[t3];
+    char Vnombrejugador1[t]={},Vnombrejugador2[t]={};
+
+     ///SALUDO AL INICIAR EL JUEGO.
+    opc=menu_inicio(Vnombrejugador1,Vnombrejugador2,t,opcion);
+    menu_juego_de_uno (Vnombrejugador1,t,punto,t2,dados,t3,opc);
+    menu_juego_de_dos(Vnombrejugador1,Vnombrejugador2,t,punto,t2,dados,t3,opc);
+   // menu_puntaje (Vnombrejugador1,Vnombrejugador2,tam1,puntosj1,puntosj2,tam2,opcion)
+    getch ();
+    return 0;
+}
